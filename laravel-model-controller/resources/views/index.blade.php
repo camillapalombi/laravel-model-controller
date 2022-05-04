312471ffb049572c48cc6c251e423d95f1126ff9
camillapalombi <camilla.palombi14@gmail.com>
2022-05-04 17:56:06 +0200

    Stampa a schermo di tutti i film

--- laravel-model-controller/resources/views/index.blade.php
+++ laravel-model-controller/resources/views/index.blade.php
@@ -1,8 +1,44 @@
 <!-- FILE INDEX LEGATO AL CONTROLLER!-->
- <ul>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Movies</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
 
-@foreach ($movies as $movie)
-     <li> {{ $movie->title }}</li>
- @endforeach
+    <header>
+        <h1>I film più belli di sempre</h1>
+    </header>
 
- </ul>
+    <main>
+        <div class="container-all-cards">
+
+
+            @foreach ($movies as $movie)
+                <div class="single-card">
+                    <ul>
+                        <li> <h3> Titolo: {{ $movie->title }} </h3> </li>
+                        <li> <h4> Titolo originale: {{ $movie->original_title }} </h4> </li>
+                        <li> Nazionalità: {{ $movie->nationality }}</li>
+                        <li> Data creazione: {{ $movie->date }}</li>
+                        <li> Voto: {{ $movie->vote }}</li>
+                    </ul>
+                </div>
+            @endforeach
+
+        </div>
+    </main>
+ 
+    
+
+
+
+
+
+
+</body>
+</html>
