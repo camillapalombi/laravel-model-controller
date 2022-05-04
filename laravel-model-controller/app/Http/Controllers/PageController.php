bd6ded7844309c2a4dee791ffa1b35aa9421d35d
camillapalombi <camilla.palombi14@gmail.com>
2022-05-04 17:13:10 +0200

    Creato Model

--- laravel-model-controller/app/Http/Controllers/PageController.php
+++ laravel-model-controller/app/Http/Controllers/PageController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Movie;
 
 class PageController extends Controller
 {
     public function index() {
         
-        //
-        //
+        $myMovies = Movie::all();
+        dd($myMovies);
 
         return view('index');
     }
